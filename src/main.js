9b67b7947e80c7393084ef4638032a78e3eb6260
starzyn <starzyn@qq.com>
2020-06-27 17:14:28 +0800

    引入了 element-ui 和 axios 插件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import ElementUi from 'element-ui'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(ElementUi)
+Vue.use(axios)
+Vue.use(VueAxios)
 
 new Vue({
   router,
